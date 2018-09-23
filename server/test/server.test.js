c6f81e2a0aa849f66d195d7574d328be619a6301
papaponmx <jaimemadrigalrios@gmail.com>
2018-09-23 12:42:24 -0500

    Added GET /todos tests

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -55,6 +55,17 @@ describe('POST /todos', () => {
        }).catch(err => done(err));
      })
   });
+});
 
 
+describe('GET /todos', () => {
+  it('Should get all todos', (done) => {
+    request(app)
+    .get('/todos')
+    .expect(200)
+    .expect(res => {
+      expect(res.body.todos.length).toBe(2)
+    })
+    .end(done)
+  });
 });
