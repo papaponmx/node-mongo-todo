45caf2686c22cd9a8300112513b57c3094ddf027
papaponmx <jaimemadrigalrios@gmail.com>
2018-09-23 18:01:45 -0500

    Added test GET /todos/:id

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -1,12 +1,19 @@
 const expect = require('expect');
 const request = require('supertest');
+const { ObjectID } = require('mongodb')
 
 const { app } = require('../server');
 const { TodoModel } = require('../models/todo');
 
 const initialTodos = [
-  { text: 'First Initial Todo'},
-  { text: 'Second Initial Todo'}
+  { 
+    _id: new ObjectID(),
+    text: 'First Initial Todo'
+  },
+  { 
+    _id: new ObjectID(),
+    text: 'Second Initial Todo'
+  },
 ]
 
 beforeEach(done => {
@@ -67,5 +74,18 @@ describe('GET /todos', () => {
       expect(res.body.todos.length).toBe(2)
     })
     .end(done)
+  });
+});
+
+
+describe('GET /todos/:id', () => {
+  it('should return todo doc', (done) => {
+    request(app)
+     .get(`/todos/${initialTodos[0]._id.toHexString()}`)
+     .expect(200)
+     .expect(res => {
+       expect(res.body.todo.text).toBe(initialTodos[0].text)
+      })
+      .end(done);
   });
 });
