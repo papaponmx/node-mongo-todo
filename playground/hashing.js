151b7b3e7fd8840c79650bb5e438873e286f3503
papaponmx <jaimemadrigalrios@gmail.com>
2018-09-30 21:39:59 -0500

    Added jwt

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,25 +1,38 @@
 const { SHA256 } = require('crypto-js');
-
-const message = 'I am user number 3';
-const hash = SHA256(message).toString();
-
-console.log(`Message: ${message}`);
-console.log(`Hash: ${hash}`);
-
+const jwt = require('jsonwebtoken')
 
 const data = {
-  id: 4,
-};
-
-const token = {
-  data,
-  hash: SHA256(JSON.stringify(data) + 'somestring').toString()
+  id: 10,
 }
 
-const resultHash = SHA256(JSON.stringify(token.data) + 'somestring altered').toString();
+const token = jwt.sign(data, '123abc');
 
-if ( resultHash === token.hash){
-  console.log('Data was not changed');
-} else {
-  console.log('Data was changed. Do not trust'); 
-}
+console.log(token);
+
+const decoded = jwt.verify(token, '123abc');
+
+console.log('decoded', decoded)
+
+// const message = 'I am user number 3';
+// const hash = SHA256(message).toString();
+
+// console.log(`Message: ${message}`);
+// console.log(`Hash: ${hash}`);
+
+
+// const data = {
+//   id: 4,
+// };
+
+// const token = {
+//   data,
+//   hash: SHA256(JSON.stringify(data) + 'somestring').toString()
+// }
+
+// const resultHash = SHA256(JSON.stringify(token.data) + 'somestring altered').toString();
+
+// if ( resultHash === token.hash){
+//   console.log('Data was not changed');
+// } else {
+//   console.log('Data was changed. Do not trust'); 
+// }
