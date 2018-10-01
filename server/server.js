00271c18a87393dab087f40ce7ec91b779496681
papaponmx <jaimemadrigalrios@gmail.com>
2018-09-30 22:50:05 -0500

    Added first private route

--- server/server.js
+++ server/server.js
@@ -110,6 +110,21 @@ app.post('/users', (req, res) => {
   })
 });
 
+app.get('/users/me', (req, res) => {
+  const token = req.header('x-auth')
+
+  User.findByToken(token)
+    .then(user => {
+      if(!user) {
+        return Promise.reject();
+      }
+
+      res.send(user);
+    }).catch(e => {
+      res.status(401).send()
+    });
+})
+
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
 });
